68d3e962419c087e87d36e29e0f6cd3edc5d2572
Байкалов Евгений <baykalov_ep@m.ru>
2023-04-17 18:42:59 +0400

    Литералы

---
+++ Chapter 02/Литералы/Program.cs
@@ -0,0 +1,45 @@
+﻿// Логические литералы
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// Целочисленные литера
+Console.WriteLine(-11);
+Console.WriteLine(5);
+Console.WriteLine(505);
+
+// Бинарные литералы
+Console.WriteLine(0b11);        // 3
+Console.WriteLine(0b1011);      // 11
+Console.WriteLine(0b100001);    // 33
+
+// Шестнадцатеричные литералы
+Console.WriteLine(0x0A);    // 10
+Console.WriteLine(0xFF);    // 255
+Console.WriteLine(0xA1);    // 161
+
+// Вещественные литералы
+Console.WriteLine(3.14);
+Console.WriteLine(100.001);
+Console.WriteLine(-0.38);
+Console.WriteLine(3.2e3);   // по сути равно 3.2 * 10 в степени 3 = 3200
+Console.WriteLine(1.2E-1);  // равно 1.2 * 10 в степени -1 = 0.12
+
+// Символьные литералы
+Console.WriteLine('2');
+Console.WriteLine('A');
+Console.WriteLine('T');
+
+Console.WriteLine('\x78');    // x
+Console.WriteLine('\x5A');    // Z
+
+Console.WriteLine('\u0420');    // Р
+Console.WriteLine('\u0421');    // С
+
+// Строковые литералы
+Console.WriteLine("hello");
+Console.WriteLine("фыва");
+Console.WriteLine("hello word");
+Console.WriteLine("Компания \"Рога и копыта\"");
+Console.WriteLine("Привет \nмир");
+
+// Литерал null
