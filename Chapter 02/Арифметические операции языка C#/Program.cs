588a2673d1085ccb4759f43cd2977c7553ab5939
Байкалов Евгений <baykalov_ep@m.ru>
2023-04-17 21:12:36 +0400

    Арифметические операции языка C#

---
+++ Chapter 02/Арифметические операции языка C#/Program.cs
@@ -0,0 +1,54 @@
+﻿//int x = 10;
+//int z = x + 12; // 22
+
+//int x = 10;
+//int z = x - 6; // 4
+
+//int x = 10;
+//int z = x * 5; // 50
+
+//int x = 10;
+//int z = x / 5; // 2
+
+//double a = 10;
+//double b = 3;
+//double c = a / b; // 3.33333333
+
+//double z = 10 / 4; //результат равен 2
+
+//double z = 10.0 / 4.0; //результат равен 2.5
+
+//double x = 10.0;
+//double z = x % 4.0; //результат равен 2
+
+//int x1 = 5;
+//int z1 = ++x1; // z1=6; x1=6
+//Console.WriteLine($"{x1} - {z1}");
+
+//int x2 = 5;
+//int z2 = x2++; // z2=5; x2=6
+//Console.WriteLine($"{x2} - {z2}");
+
+//int x1 = 5;
+//int z1 = --x1; // z1=4; x1=4
+//Console.WriteLine($"{x1} - {z1}");
+
+//int x2 = 5;
+//int z2 = x2--; // z2=5; x2=4
+//Console.WriteLine($"{x2} - {z2}");
+
+//int a = 3;
+//int b = 5;
+//int c = 40;
+//int d = c-- - b * a;    // a=3  b=5  c=39  d=25
+//Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+
+//int d = (c--) - (b * a);
+
+//int a = 3;
+//int b = 5;
+//int c = 40;
+//int d = (c - (--b)) * a;    // a=3  b=4  c=40  d=108
+//Console.WriteLine($"a={a}  b={b}  c={c}  d={d}");
+
+//int x = 10 / 5 * 2;
--- Chapter 02/Консольный ввод-вывод/Program.cs
+++ Chapter 02/Консольный ввод-вывод/Program.cs
@@ -4,6 +4,7 @@
 //Console.WriteLine("Пока мир...");
 //Console.WriteLine(24.5);
 
+// Интерполяция - встраивание переменных в строки
 //string name = "Tom";
 //int age = 34;
 //double height = 1.7;
@@ -14,6 +15,20 @@
 //double height = 1.7;
 //Console.WriteLine("Имя: {0}  Возраст: {2}  Рост: {1}м", name, height, age);
 
-Console.Write("Введите свое имя: ");
+//Console.Write("Введите свое имя: ");
+//string? name = Console.ReadLine();
+//Console.WriteLine($"Привет {name}");
+
+Console.Write("Введите имя: ");
 string? name = Console.ReadLine();
-Console.WriteLine($"Привет {name}");
+
+Console.Write("Введите возраст: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите рост: ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите размер зарплаты: ");
+decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
