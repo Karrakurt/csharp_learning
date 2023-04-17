855d1ab2cdbd619393e3c8ceb38732c7d57c7955
Байкалов Евгений <baykalov_ep@m.ru>
2023-04-17 18:25:07 +0400

    Переменные и константы

---
+++ Chapter 02/Переменные и константы/Program.cs
@@ -0,0 +1,8 @@
+﻿const string NAME = "Tom";  // определяем константу
+string name = "Tom";  // определяем переменную и инициализируем ее
+
+Console.WriteLine(name);    // Tom
+
+name = "Bob";       // меняем значение переменной
+Console.WriteLine(name);    // Bob
+Console.WriteLine(NAME);    // Tom
