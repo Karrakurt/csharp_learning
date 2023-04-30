b87e7e3bbab08012f8b134ddb553c4060f2d097f
Байкалов Евгений <4registrationz@mail.ru>
2023-05-01 01:07:33 +0400

    Chapter 02\Преобразования базовых типов данных
    Ключевое слово checked для проверки выражения на переполнение
    Ключевое слово unchecked для игнорирования выражения на переполнение

---
+++ Chapter 02/Преобразования базовых типов данных/Program.cs
@@ -0,0 +1,26 @@
+﻿//byte a = 4;
+//byte b = (byte)(a + 70);
+
+//byte a = 4;             // 0000100
+//ushort b = a;   // 000000000000100
+
+//ushort a = 4;
+//byte b = (byte)a;
+
+//sbyte a = 4;            // 0000100
+//short b = a;    // 000000000000100
+
+//sbyte a = -4;           // 1111100
+//short b = a;    // 111111111111100
+
+try
+{
+    int a = 33;
+    int b = 600;
+    byte c = checked((byte)(a + b));
+    Console.WriteLine(c);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex.Message);
+}
