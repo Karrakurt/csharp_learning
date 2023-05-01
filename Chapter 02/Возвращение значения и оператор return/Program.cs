f86413380a3c29a35fa6b111613e0a01d768fa5a
Байкалов Евгений <4registrationz@mail.ru>
2023-05-02 03:03:58 +0400

    Параметры по ссылке и значению
    При использовании в списке параметров метода ключевое слово ref указывает на то, что аргумент передается по ссылке, а не по значению
    Определение переменных возможно непосредственно при вызове методов с выходными параметрами

---
+++ Chapter 02/Возвращение значения и оператор return/Program.cs
@@ -0,0 +1,63 @@
+﻿//return возвращаемое значение;
+
+string GetMessage()
+{
+    return "Hello";
+}
+
+//string GetMessage()
+//{
+//    Console.WriteLine("Hello"); //Ошибка, метод должен возвращать результат!
+//}
+
+//string GetMessage()
+//{
+//    return 3;   // Ошибка! Метод должен возвращать строку, а не число
+//}
+
+string message = GetMessage();  // получаем результат метода в переменную message
+Console.WriteLine(message);     // Hello
+
+void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+PrintMessage(GetMessage());
+
+int Sum(int x, int y)
+{
+    return x + y;
+}
+int result = Sum(10, 15);   // 25
+Console.WriteLine(result);   // 25
+Console.WriteLine(Sum(5, 6));   // 11
+
+
+//Сокращенная версия методов с результатом
+
+
+string GetMessageShort() => "hello";
+int SumShort(int x, int y) => x + y;
+
+
+//Выход из метода
+
+
+string GetHello()
+{
+    return "Hello";
+    Console.WriteLine("After return");
+}
+
+void PrintPerson(string name, int age)
+{
+    if (age > 120 || age < 1)
+    {
+        Console.WriteLine("Недопустимый возраст");
+        return;
+    }
+    Console.WriteLine($"Имя: {name}  Возраст: {age}");
+}
+
+PrintPerson("Tom", 37);         // Имя: Tom  Возраст: 37
+PrintPerson("Dunkan", 1234);    // Недопустимый возраст
---
+++ Chapter 02/Параметры по ссылке и значению/Program.cs
@@ -0,0 +1,84 @@
+﻿//Передача параметров по значению
+
+
+void Increment(int n)
+{
+    n++;
+    Console.WriteLine($"Число в методе Increment: {n}");
+}
+
+int number = 5;
+Console.WriteLine($"Число до метода Increment: {number}");
+Increment(number);
+Console.WriteLine($"Число после метода Increment: {number}");
+
+
+//Передача параметров по ссылке и модификатор ref
+
+
+void IncrementRef(ref int n)
+{
+    n++;
+    Console.WriteLine($"Число в методе Increment: {n}");
+}
+
+int numberRef = 5;
+Console.WriteLine($"Число до метода Increment: {numberRef}");
+IncrementRef(ref numberRef);
+Console.WriteLine($"Число после метода Increment: {numberRef}");
+
+
+//Выходные параметры. Модификатор out
+
+
+void Sum(int x, int y, out int result)
+{
+    result = x + y;
+}
+int numberOut;
+Sum(10, 15, out numberOut);
+Console.WriteLine(numberOut);   // 25
+
+//void SumOut2(int x, int y, out int result)
+//{
+//    Console.WriteLine(x + y); //Ошибка! Параметру result не присвоено значение
+//}
+
+void GetRectangleData(int width, int height, out int rectArea, out int rectPerimetr)
+{
+    rectArea = width * height;       // площадь прямоугольника - произведение ширины на высоту
+    rectPerimetr = (width + height) * 2; // периметр прямоугольника - сумма длин всех сторон  
+}
+int area;
+int perimetr;
+GetRectangleData(10, 20, out area, out perimetr);
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+
+GetRectangleData(10, 20, out int areaInLine, out int perimetrInLine);
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+
+GetRectangleData(10, 20, out var areaVar, out var perimetrVar);
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+
+
+//Входные параметры. Модификатор in
+
+
+void GetRectangleDataIn(in int width, in int height, out int rectArea, out int rectPerimetr)
+{
+    //width = 25; // нельзя изменить, так как width - входной параметр
+    rectArea = width * height;
+    rectPerimetr = (width + height) * 2;
+}
+int w = 10;
+int h = 20;
+GetRectangleDataIn(w, h, out var areaIn, out var perimetrIn);
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+
+
+//При использовании в списке параметров метода ключевое слово ref указывает на то, что аргумент передается по ссылке, а не по значению
+//Определение переменных возможно непосредственно при вызове методов с выходными параметрами
