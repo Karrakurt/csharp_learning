070702fefb78aa67b66c836e239f65835b407fb4
Байкалов Евгений <4registrationz@mail.ru>
2023-05-01 22:00:45 +0400

    Циклы
    В цикле for допустим пропуск блоков, в которых нет необходимости

---
+++ Chapter 02/Конструкция if else и тернарная операция/Program.cs
@@ -0,0 +1,79 @@
+﻿//if (условие)
+//{
+//    выполняемые инструкции
+//}
+
+//int num1 = 8;
+//int num2 = 6;
+//if (num1 > num2)
+//{
+//    Console.WriteLine($"Число {num1} больше числа {num2}");
+//}
+
+//int num1 = 8;
+//int num2 = 6;
+//if (num1 > num2)
+//    Console.WriteLine($"Число {num1} больше числа {num2}");
+
+//// или так
+//if (num1 > num2) Console.WriteLine($"Число {num1} больше числа {num2}");
+
+//int num1 = 8;
+//int num2 = 6;
+//if (num1 > num2 && num1 == 8)
+//{
+//    Console.WriteLine($"Число {num1} больше числа {num2}");
+//}
+
+//Выражение else
+//int num1 = 8;
+//int num2 = 6;
+//if (num1 > num2)
+//{
+//    Console.WriteLine($"Число {num1} больше числа {num2}");
+//}
+//else
+//{
+//    Console.WriteLine($"Число {num1} меньше числа {num2}");
+//}
+
+//int num1 = 8;
+//int num2 = 6;
+//if (num1 > num2)
+//    Console.WriteLine($"Число {num1} больше числа {num2}");
+//else
+//    Console.WriteLine($"Число {num1} меньше числа {num2}");
+
+//else if
+int num1 = 8;
+int num2 = 6;
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+else
+{
+    Console.WriteLine("Число num1 равно числу num2");
+}
+
+string name = "Alex";
+
+if (name == "Tom")
+    Console.WriteLine("Вас зовут Tomas");
+else if (name == "Bob")
+    Console.WriteLine("Вас зовут Robert");
+else if (name == "Mike")
+    Console.WriteLine("Вас зовут Michael");
+else
+    Console.WriteLine("Неизвестное имя");
+
+//Тернарная операция
+int x = 3;
+int y = 2;
+
+int z = x < y ? (x + y) : (x - y);
+Console.WriteLine(z);   // 1
---
+++ Chapter 02/Циклы/Program.cs
@@ -0,0 +1,137 @@
+﻿
+
+//Цикл for
+
+
+//for ([действия_до_выполнения_цикла]; [условие] ; [действия_после_выполнения])
+//{
+//    // действия
+//}
+
+//for (int i = 1; i < 4; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for (int i = 1; i < 4; i++)
+//    Console.WriteLine(i);
+
+//// или так
+//for (int i = 1; i < 4; i++) Console.WriteLine(i);
+
+//var i = 1;
+//for (Console.WriteLine("Начало выполнения цикла"); i < 4; Console.WriteLine($"i = {i}"))
+//{
+//    i++;
+//}
+
+//int i = 1;
+//for (; ; )
+//{
+//    Console.WriteLine($"i = {i}");
+//    i++;
+//}
+
+//int i = 1;
+//for (; i < 4;)
+//{
+//    Console.WriteLine($"i = {i}");
+//    i++;
+//}
+
+//for (int i = 1, j = 1; i < 10; i++, j++)
+//    Console.WriteLine($"{i * j}");
+
+
+//Цикл do..while
+
+
+//do
+//{
+//    действия цикла
+//}
+//while (условие)
+
+//int i = 6;
+//do
+//{
+//    Console.WriteLine(i);
+//    i--;
+//}
+//while (i > 0);
+
+//int i = -1;
+//do
+//{
+//    Console.WriteLine(i);
+//    i--;
+//}
+//while (i > 0);
+
+
+//Цикл while
+
+
+//while (условие)
+//{
+//    действия цикла
+//}
+
+//int i = 6;
+//while (i > 0)
+//{
+//    Console.WriteLine(i);
+//    i--;
+//}
+
+
+//Цикл foreach
+
+
+//foreach (тип_данных переменная in коллекция)
+//{
+//    // действия цикла
+//}
+
+foreach (char c in "Tom")
+{
+    Console.WriteLine(c);
+}
+
+foreach (var c in "Tom")
+{
+    Console.WriteLine(c);
+}
+
+
+//Операторы continue и break
+
+
+for (int i = 0; i < 9; i++)
+{
+    if (i == 5)
+        break;
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 9; i++)
+{
+    if (i == 5)
+        continue;
+    Console.WriteLine(i);
+}
+
+
+//Вложенные циклы
+
+
+for (int i = 1; i < 10; i++)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        Console.Write($"{i * j} \t");
+    }
+    Console.WriteLine();
+}
+
+//В цикле for допустим пропуск блоков, в которых нет необходимости
